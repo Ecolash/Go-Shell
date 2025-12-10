8245aeeb918abd29aecbfcc3c9b2d30976f6e86e
Ecolash <codeandcode0@gmail.com>
2025-12-10 19:01:25 +0100

    Implement echo

--- internal/exec/builtin.go
+++ internal/exec/builtin.go
@@ -3,23 +3,38 @@ package exec
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/codecrafters-io/shell-starter-go/internal/parser"
 )
 
+const (
+	BuiltinCD    = "cd"
+	BuiltinExit  = "exit"
+	BuiltinPWD   = "pwd"
+	BuiltinEcho  = "echo"
+	BuiltinSet   = "set"
+	BuiltinUnset = "unset"
+	BuiltinEnv   = "env"
+)
+
 func builtin(cmd *parser.Command) bool {
 	switch cmd.Name {
-	case "cd":
-		if len(cmd.Args) == 0 {
-			fmt.Println("cd: missing argument")
-			return true
-		}
-		if err := os.Chdir(cmd.Args[0]); err != nil {
-			fmt.Println("cd error:", err)
-		}
+	//case BuiltinCD:
+	//	if len(cmd.Args) == 0 {
+	//		fmt.Println("cd: missing argument")
+	//		return true
+	//	}
+	//	if err := os.Chdir(cmd.Args[0]); err != nil {
+	//		fmt.Println("cd error:", err)
+	//	}
+	//	return true
+
+	case BuiltinEcho:
+		fmt.Println(strings.Join(cmd.Args, " "))
 		return true
 
-	case "exit":
+	case BuiltinExit:
 		os.Exit(0)
 		return true
 	}
