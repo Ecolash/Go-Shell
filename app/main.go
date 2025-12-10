d344266cb30a01628b9c202ab2e169404840fe08
Ecolash <codeandcode0@gmail.com>
2025-12-10 18:32:18 +0100

    Invalid command done

--- app/main.go
+++ app/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -9,4 +12,12 @@ var _ = fmt.Print
 
 func main() {
 	fmt.Print("$ ")
+	reader := bufio.NewReader(os.Stdin)
+	command, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	command = strings.TrimSuffix(command, "\n")
+	fmt.Println(command + ": command not found")
 }
