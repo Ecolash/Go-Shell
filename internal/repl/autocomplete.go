1b33d1856369a568420fe5ef4f5509c1e6710888
Ecolash <codeandcode0@gmail.com>
2025-12-10 23:54:53 +0100

    Add path prefix finder

--- internal/repl/autocomplete.go
+++ internal/repl/autocomplete.go
@@ -2,6 +2,8 @@ package repl
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"strings"
 
 	"github.com/codecrafters-io/shell-starter-go/internal/exec"
@@ -20,11 +22,42 @@ var builtinNames = []string{
 
 type builtinCompleter struct{}
 
+func loadPathExecutables() []string {
+	path := os.Getenv("PATH")
+	dirs := filepath.SplitList(path)
+	seen := map[string]bool{}
+	var out []string
+
+	for _, d := range dirs {
+		entries, err := os.ReadDir(d)
+		if err != nil {
+			continue
+		}
+		for _, e := range entries {
+			if !e.Type().IsRegular() {
+				continue
+			}
+			name := e.Name()
+			if !seen[name] {
+				seen[name] = true
+				out = append(out, name)
+			}
+		}
+	}
+	return out
+}
+
 func (c *builtinCompleter) Do(line []rune, _ int) ([][]rune, int) {
 	prefix := string(line)
 	var matches []string
 
 	for _, b := range builtinNames {
+		if strings.HasPrefix(b, prefix) {
+			matches = append(matches, b)
+		}
+	}
+
+	for _, b := range loadPathExecutables() {
 		if strings.HasPrefix(b, prefix) {
 			matches = append(matches, b)
 		}
