0bff5350f2d064d6aceb65a0b1a3153820402f3f
Ecolash <codeandcode0@gmail.com>
2025-12-11 12:43:39 +0100

    Print file paths

--- internal/repl/repl.go
+++ internal/repl/repl.go
@@ -10,14 +10,27 @@ import (
 	"github.com/codecrafters-io/shell-starter-go/internal/parser"
 )
 
+func formatPrompt() string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		cwd = "?"
+	}
+	home := os.Getenv("HOME")
+	if home != "" && strings.HasPrefix(cwd, home) {
+		cwd = "~" + strings.TrimPrefix(cwd, home)
+	}
+	return fmt.Sprintf("%s $ ", cwd)
+}
+
 func Start() {
 	histfile := os.Getenv("HISTFILE")
 	if histfile == "" {
 		histfile = "/tmp/my_shell_history.tmp"
 	}
+
 	SetupHistory()
 	rl, err := readline.NewEx(&readline.Config{
-		Prompt:          "$ ",
+		Prompt:          formatPrompt(),
 		InterruptPrompt: "^C",
 		EOFPrompt:       "exit",
 		AutoComplete:    &builtinCompleter{},
@@ -28,13 +41,15 @@ func Start() {
 		return
 	}
 	defer rl.Close()
+
 	for {
+		rl.SetPrompt(formatPrompt())
 		input, err := rl.Readline()
 		exec.ShellHistory = append(exec.ShellHistory, input)
-
 		if err != nil { // EOF or Ctrl+C
 			break
 		}
+
 		input = strings.TrimSpace(input)
 		if input == "" {
 			continue
@@ -45,9 +60,11 @@ func Start() {
 			fmt.Println("parse error:", err)
 			continue
 		}
+
 		if command == exec.BuiltinExit {
 			UpdateHistory()
 		}
+
 		exec.Run(command)
 	}
 }
